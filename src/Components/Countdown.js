6045f19758142b6728d8f3bac8d53ce7a7e3491f
Luke Ketchen <luke.ketchen@gmail.com>
2020-01-28 13:25:26 +1100

    V1.0
    + countdown timer works
    + START, STOP, RESET buttons work and disabled when needed
    + Audio cues when counters stop
    
    ~ still need to add css to make it all look pretty, Going to look for a design to get some inspiration

--- src/Components/Countdown.js
+++ src/Components/Countdown.js
@@ -1,61 +1,108 @@
 import React from "react";
+import cheerSound from "../assets/sounds/cheer.wav";
+import warHornSound from "../assets/sounds/warhorn.wav";
 
 class Countdown extends React.Component {
   constructor() {
     super();
-    this.state = { currentTime: "00:00", timerTime: 1, timerState: false };
-    this.start = new Date().getTime();
+    this.state = {
+      currentTime: "00:00",
+      timerTime: 1,
+      timerState: false,
+      stoppedState: false,
+      currentMode: true
+    };
+    this.timer = -1;
   }
 
   startTimer = () => {
     if (!this.state.timerState) {
-      this.setState({ timerState: true });
+      this.setState({ timerState: true, stoppedState: false });
     } else {
-      this.setState({ timerState: false });
+      this.setState({ timerState: false, stoppedState: true });
     }
-
-    const countDownDate = new Date(
-      this.start + this.state.timerTime * 60000
+    this.start = new Date().getTime();
+    this.countDownDate = new Date(
+      this.start + this.getCurrentMode() * 60000
     ).getTime();
 
-    this.mytimer = setInterval(() => {
-      if (this.state.timerState) {
+    if (this.timer === -1) {
+      this.timer = setInterval(() => {
         var now = new Date().getTime();
-        var distance = countDownDate - now;
+        var distance = this.countDownDate - now;
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
         var output = (minutes > 0 ? minutes : "0" + minutes) + ":" + seconds;
         this.setState({ currentTime: output });
-        console.log(this.state.currentTime);
 
         if (distance < 0) {
-          clearInterval(this.mytimer);
-          this.setState({ currentTime: "00:00" });
-          console.log("EXPIRED");
+          if (this.state.currentMode) {
+            this.playSound(cheerSound);
+          } else {
+            this.playSound(warHornSound);
+          }
+          clearInterval(this.timer);
+          this.timer = -1;
+          this.setState(prevState => ({
+            currentTime: "00:00",
+            timerState: false,
+            currentMode: !prevState.currentMode
+          }));
         }
-      } else if (this.state.timerState === false) {
-        clearInterval(this.mytimer);
-      }
-    }, 1000);
+      }, 1000);
+    } else {
+      clearInterval(this.timer);
+      this.timer = -1;
+    }
+  };
+
+  playSound = soundName => {
+    const sound = new Audio(soundName);
+    sound.play();
+  };
+
+  getCurrentMode = () => {
+    if (this.state.currentMode) {
+      return 1;
+    } else {
+      return 2;
+    }
   };
 
   stopTimer = () => {
-    this.setState({ currentTime: "00:00" });
-    this.setState({ timerState: false });
+    this.setState({
+      currentTime: "00:00",
+      timerState: false,
+      stoppedState: true
+    });
+    clearInterval(this.timer);
+    this.timer = -1;
+  };
+
+  handleReset = () => {
+    this.setState({
+      timerTime: 1,
+      currentMode: true
+    });
   };
 
   render() {
     return (
       <div>
         <h1>Timer Div</h1>
+        {this.state.currentMode ? <h2>52</h2> : <h2>17</h2>}
         <h2>{this.state.currentTime}</h2>
-
-        <button onClick={() => this.startTimer()}>
-          {this.state.timerState ? "Pause" : "Start"}
-        </button>
-
+        {this.state.timerState ? (
+          <button disabled>START</button>
+        ) : (
+          <button onClick={() => this.startTimer()}>START</button>
+        )}
         <button onClick={this.stopTimer}>STOP</button>
+        {this.state.stoppedState ? (
+          <button onClick={this.handleReset}>RESET</button>
+        ) : (
+          <button disabled>RESET</button>
+        )}
       </div>
     );
   }
